8daa21dcdcba5cfc251c1cd03e56b47c14858d09
Luis Alejandro Ramirez Arce <alejandro.arce@alejandro.arce>
2021-04-29 01:04:44 -0500

    adding lintern

---
+++ test/specs/taskTest.js
@@ -0,0 +1,29 @@
+
+import LoginPage from '../pages/loginPage'
+import NotePage from '../pages/notePage'
+import * as constants from '../utils/constants'
+import allureReporter from '@wdio/allure-reporter'
+
+describe ('Task Creation', () => {
+    before(()=>{
+        LoginPage.open();
+        LoginPage.login(constants.EMAIL, constants.PASSWORD);
+    })
+
+    it('create 1 task', () => {  
+        allureReporter.addFeature('create 1 task')    
+        let taskName=constants.TASKNAME+new Date().getMilliseconds();
+        NotePage.createNewTask(taskName);
+        expect(NotePage.getTaskByName(taskName)).toHaveText(taskName);
+    });
+
+    it('create 10 task', () => {
+        allureReporter.addFeature('create 10 task')   
+        NotePage.createTasks(10);
+        for(let i=1;i<10;i++){
+        expect(NotePage.getTaskByName("taskName" + i)).toHaveText( "taskName" + i)
+        }
+
+    });
+
+});
