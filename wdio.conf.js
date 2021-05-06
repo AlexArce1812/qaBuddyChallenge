bdc73b43af7e12b9f9a844ec0f6991a19764ab23
Luis Alejandro Ramirez Arce <alejandro.arce@alejandro.arce>
2021-05-06 15:36:57 -0500

    Adding saucelabs

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome', 
-        platformName: 'Windows 10',
+   /*     platformName: 'Windows 10',
         browserVersion: 'latest',
         acceptInsecureCerts: true,
          'sauce: options':{
@@ -75,7 +75,7 @@ exports.config = {
          'sauce: options':{
             extendedDebugging: true
           },
-    }
+    }*/
 
     
 
@@ -127,7 +127,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    user: process.env.SAUCE_USERNAME,
+   /* user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
     services: [
         ['sauce', {
@@ -135,9 +135,9 @@ exports.config = {
             sauceConnect: false,
             sauceConnectOpts: {}
             }]
-    ],
+    ],*/
     
-    //services: ['chromedriver'],
+    services: ['chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
