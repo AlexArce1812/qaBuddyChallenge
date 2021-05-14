b369ccb20e88ddf7943db29ade500680242713ec
Luis Alejandro Ramirez Arce <alejandro.arce@alejandro.arce>
2021-05-14 13:43:50 -0500

    running CI with slack notifications

--- test/utils/constants.js
+++ test/utils/constants.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const EMAIL = process.env.user_email
-export const PASSWORD = process.env.password_todoist
-export const WRONGEMAIL = process.env.wrong_user_email
-export const WRONGPASSWORD = process.env.wrong_password
+export const EMAIL = process.env.USER_EMAIL
+export const PASSWORD = process.env.PASSWORD_TODOIST
+export const WRONGEMAIL = process.env.WRONG_USER_EMAIL
+export const WRONGPASSWORD = process.env.WRONG_PASSWORD
 export const EMPTY=""
-export const TASKNAME=process.env.task_name
+export const TASKNAME=process.env.TASK_NAME
