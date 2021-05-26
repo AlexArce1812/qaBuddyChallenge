be13aaa044f795fdb1a32152fd2651a2009b6dff
Luis Alejandro Ramirez Arce <alejandro.arce@alejandro.arce>
2021-05-26 12:28:35 -0500

    running CI with slack notifications

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,15 +55,15 @@ exports.config = {
         maxInstances: 5,
         //
        browserName: 'chrome',
-        'goog:chromeOptions': {
+      /*  'goog:chromeOptions': {
             args: ['headless', 'disable-gpu'],
-          },/*
+          },*/
         platformName: 'Windows 10',
         browserVersion: 'latest',
         acceptInsecureCerts: true,
          'sauce: options':{
             extendedDebugging: true
-          }*/
+          }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
